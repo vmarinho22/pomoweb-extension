12c4aa75e8fcefc3290e1e1553014f8cdf2bb8ba
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2023-08-11 01:15:57 -0300

    feat: start project

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,10 +1,10 @@
 import { CountButton } from "~features/count-button"
 
-import "~style.css"
+import "~styles/global.css"
 
 function IndexPopup() {
   return (
-    <div className="plasmo-flex plasmo-items-center plasmo-justify-center plasmo-h-16 plasmo-w-40">
+    <div className="flex items-center justify-center h-16 w-40">
       <CountButton />
     </div>
   )
